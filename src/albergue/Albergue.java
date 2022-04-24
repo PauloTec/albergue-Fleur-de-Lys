3bbe265cdb0b9ac9620ecdd7accab29e24b4e37b
Diassilua Paulo Simão <diassilua.paulo.simao@outlook.com>
2022-04-24 02:16:33 +0100

    Menu inicial

--- src/albergue/Albergue.java
+++ src/albergue/Albergue.java
@@ -5,6 +5,8 @@
  */
 package albergue;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author BelissimaV
@@ -16,14 +18,47 @@ public class Albergue {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("1- Adicione um funcionário");
-        System.out.println("2- Adicione um pagamento");
-        System.out.println("3- Exibir o total de contribuições para o Plano de Pensão do Canadá");
-        System.out.println("4- Exibir o total de contribuições para o seguro-desemprego");
-        System.out.println("5- Lista de funcionários com taxa fixa (RG, nome e horas extras)");
-        System.out.println("6- Lista de funcionários comissionados (ID, nome e faturamento bruto)");
-        System.out.println("6- Lista de funcionários comissionados (ID, nome e faturamento bruto)");
-        System.out.println("7- Sair");
-    }
-    
+        
+        int opcao;
+        
+        do {
+            opcao = Integer.parseInt(JOptionPane.showInputDialog("Escolha um programa: "
+                + "\n 1 - 1- Adicione um funcionário."  
+                + "\n 2 - Adicione um pagamento."
+                + "\n 3 - Exibir o total de contribuições para o Plano de Pensão do Canadá."
+                + "\n 4 - Exibir o total de contribuições para o seguro-desemprego."
+                + "\n 5 - Lista de funcionários com taxa fixa (RG, nome e horas extras)."
+                + "\n 6 - Lista de funcionários comissionados (ID, nome e faturamento bruto)."
+                + "\n 7 - Sair."));
+            
+            switch (opcao) {
+                case 1:
+                        JOptionPane.showMessageDialog(null, "Escolheu a opção 1");
+                        break;
+                case 2:
+                        JOptionPane.showMessageDialog(null, "Escolheu a opção 2");
+                        break;
+                case 3:
+                        JOptionPane.showMessageDialog(null, "Escolheu a opção 3");
+                        break;
+                case 4:
+                        JOptionPane.showMessageDialog(null, "Escolheu a opção 4.");
+                        break;    
+                case 5:
+                        JOptionPane.showMessageDialog(null, "Escolheu a opção 5");
+                        break;
+                case 6:
+                        JOptionPane.showMessageDialog(null, "Escolheu a opção 6");
+                        break;    
+                        
+                case 7:
+                        JOptionPane.showMessageDialog(null, "Escolheu a opção Sair!");
+                        break; 
+                default:
+                        JOptionPane.showMessageDialog(null, "Opção invalida!");
+            }
+        } while (opcao != 7);
+                     
+        JOptionPane.showMessageDialog(null, "Programa encerrado.");
+    }  
 }
