4c01b508c9100a5a319ef7b95892392cedb51cb0
Diassilua Paulo Simão <diassilua.paulo.simao@outlook.com>
2022-04-24 01:17:37 +0100

    Criando o projecto

---
+++ src/albergue/Albergue.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package albergue;
+
+/**
+ *
+ * @author BelissimaV
+ */
+public class Albergue {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("1- Adicione um funcionário");
+        System.out.println("2- Adicione um pagamento");
+        System.out.println("3- Exibir o total de contribuições para o Plano de Pensão do Canadá");
+        System.out.println("4- Exibir o total de contribuições para o seguro-desemprego");
+        System.out.println("5- Lista de funcionários com taxa fixa (RG, nome e horas extras)");
+        System.out.println("6- Lista de funcionários comissionados (ID, nome e faturamento bruto)");
+        System.out.println("6- Lista de funcionários comissionados (ID, nome e faturamento bruto)");
+        System.out.println("7- Sair");
+    }
+    
+}
